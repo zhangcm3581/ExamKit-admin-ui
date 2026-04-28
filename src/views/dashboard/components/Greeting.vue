a07aed11be791ed04fb298ba58ddaf0e32e31ae2
neo <neo@gmail.com>
2026-04-29 04:48:19 +0800

    refactor(web-admin): 抽离首页问候卡为独立组件

---
+++ src/views/dashboard/components/Greeting.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-card shadow="never" class="mt-2">
+    <div class="flex flex-wrap">
+      <div class="flex-1 flex items-start">
+        <img
+          class="w80px h80px rounded-full"
+          :src="userStore.userInfo.avatar + '?imageView2/1/w/80/h/80'"
+        />
+        <div class="ml-5">
+          <p>{{ greetings }}</p>
+          <p class="text-sm text-gray">欢迎使用 ExamKit 在线做题系统！</p>
+        </div>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useUserStore } from "@/store/modules/user-store";
+
+const userStore = useUserStore();
+const currentDate = new Date();
+
+const greetings = computed(() => {
+  const hours = currentDate.getHours();
+  const nickname = userStore.userInfo.nickname;
+  if (hours >= 6 && hours < 8) {
+    return "晨起披衣出草堂，轩窗已自喜微凉🌅！";
+  } else if (hours >= 8 && hours < 12) {
+    return `上午好，${nickname}！`;
+  } else if (hours >= 12 && hours < 18) {
+    return `下午好，${nickname}！`;
+  } else if (hours >= 18 && hours < 24) {
+    return `晚上好，${nickname}！`;
+  } else {
+    return "偷偷向银河要了一把碎星，只等你闭上眼睛撒入你的梦中，晚安🌛！";
+  }
+});
+</script>
